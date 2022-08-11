729229111b7a21cd4438686bba3f6474756db640
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-11 20:34:08 +0200

    added progress indicators

--- data/create_text.py
+++ data/create_text.py
@@ -4,6 +4,8 @@ import argparse
 
 import pandas as pd
 
+from tqdm import tqdm
+
 from miditok import REMI
 from miditok import MidiFile
 
@@ -11,20 +13,22 @@ from miditok import MidiFile
 def main(args):
     tokenizer = REMI()
     
+    print("Loading songs...")
     paths = glob.glob(f"{args.path}/.midi")
     songs = []
-    for path in paths:
+    for path in tqdm(paths):
         midi = MidiFile(path)
         tokens = tokenizer.midi_to_tokens(midi)
         songs.append(tokens)
 
+    print("Extracting snippets...")
     metadata = {
         "filename": [],
         "tokens": [],
         "origin": []
     }
     max_songs = len(songs) - 1
-    for i in args.num_seq:
+    for i in tqdm(args.num_seq):
         song_idx = random.randint(0, max_songs)
         song_tokens = songs[song_idx][0]
         start_token_idx = random.randint(
@@ -36,7 +40,7 @@ def main(args):
 
         seq = song_tokens[start_token_idx:end_token_idx]
 
-        metadata["filename"].append(f"{str(i)}.midi")
+        metadata["filename"].append(f"{str(i)}.wav")
         metadata["tokens"].append(seq)
         metadata["origin"].append(paths[i])
 
